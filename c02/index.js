6934646e0e656057b155c873d15286e143354ac5
Vangel22 <h.vangel22@gmail.com>
2023-04-03 22:03:13 +0200

    Saving all from class 2

--- c02/index.js
+++ c02/index.js
@@ -23,9 +23,10 @@ const readData = (source) => {
 
 // test();
 
-const writeData = (data, destination) => {
+const writeData = (data, destination, typeOfData) => {
   return new Promise((success, fail) => {
-    const out = JSON.parse(data);
+    const out =
+      typeOfData === "object" ? JSON.stringify(data) : JSON.parse(data);
     fs.writeFile(`${destination}.json`, out, (err) => {
       if (err) return fail(err);
       return success();
@@ -33,7 +34,42 @@ const writeData = (data, destination) => {
   });
 };
 
+const addPerson = async (id, firstname, lastname) => {
+  try {
+    const person = {
+      id: id,
+      first_name: firstname,
+      last_name: lastname,
+    };
+    let data = await readData("./data");
+    data.push(person);
+    await writeData(data, "./data", typeof person);
+  } catch (err) {
+    throw err;
+  }
+};
+
+const removePerson = async (index) => {
+  try {
+    let data = await readData("./data");
+    let out = data.filter((user) => user.id !== index);
+    await writeData(out, "./data");
+  } catch (err) {
+    throw err;
+  }
+};
+
+//Object => JSON.stringify(obj)
+//String => JSON.parse(string)
+
+//IIFE
+(async () => {
+  await addPerson(4, "TestName", "TestSurname");
+  await removePerson(4);
+})();
+
 const express = require("express");
+const { parse } = require("path");
 const api = express();
 
 api.use(express.json());
@@ -48,15 +84,22 @@ api.get("/users", async (req, res) => {
   }
 });
 
+// call users route
+// get a single user
+// filter the user with id 2 --- this can be done multiple ways
+// show data in browser
+// display error otherwise
+
 const PORT = 10000;
 
-api.listen(PORT, (err) => {
-  if (err) return console.log(err);
-  console.log(`Server successfully started on port ${PORT}`);
-});
+// api.listen(PORT, (err) => {
+//   if (err) return console.log(err);
+//   console.log(`Server successfully started on port ${PORT}`);
+// });
 
 //Homework
 // 1. Install dotenv
 // 2. Create .env file in root directory
 // 3. process.env
 // Link to docs: https://www.npmjs.com/package/dotenv
+//Extend addPerson function to throw error if there is already user with the entered ID
