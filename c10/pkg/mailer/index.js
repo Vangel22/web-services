878b984d27ad11b50a35b2431dcf46752afdcf9a
Vangel22 <h.vangel22@gmail.com>
2023-05-08 21:04:05 +0200

    fixing mailer

--- c10/pkg/mailer/index.js
+++ c10/pkg/mailer/index.js
@@ -1,26 +1,7 @@
 const fs = require("fs");
 const formData = require("form-data");
-const config = require("../config");
-const Mailgun = require("mailgun").Mailgun;
-
-const formData = require("form-data");
 const Mailgun = require("mailgun.js");
-const mailgun = new Mailgun(formData);
-const mg = mailgun.client({
-  username: "api",
-  key: process.env.MAILGUN_API_KEY || "key-yourkeyhere",
-});
-
-mg.messages
-  .create("sandbox-123.mailgun.org", {
-    from: "Excited User <mailgun@sandbox-123.mailgun.org>",
-    to: ["test@example.com"],
-    subject: "Hello",
-    text: "Testing some Mailgun awesomeness!",
-    html: "<h1>Testing some Mailgun awesomeness!</h1>",
-  })
-  .then((msg) => console.log(msg)) // logs response data
-  .catch((err) => console.log(err)); // logs any error
+const config = require("../config");
 
 const mailTemplates = {
   PASSWORD_RESET: {
@@ -34,17 +15,22 @@ const mailTemplates = {
 };
 
 const sendMail = async (to, type, data) => {
-  const mailgun = new Mailgun(config.get("service").api_key);
-  console.log("mailgun", mailgun);
+  // data: {first_name: 'Bojan', last_name: 'Gavrovski', email: 'bojan@gmail.com'}
+  const mailgun = new Mailgun(formData);
+  const mg = mailgun.client({
+    username: "api",
+    key: config.get("service").api_key,
+  });
 
   let title = mailTemplates[type].title;
+
   let templatePath = `${__dirname}/../../email_templates/${mailTemplates[type].template}`;
   let content = await readTemplate(templatePath);
 
-  //   for (let i in data) {
-  //     let regex = new RegExp(`\{\{${i}\}\}`, "g"); // {{first_name}} // {{last_name}} // {{email}}
-  //     content = content.replace(regex, data[i]); // Bojan // Gavrovski // bojan@gmail.com
-  //   }
+  for (let i in data) {
+    let regex = new RegExp(`\{\{${i}\}\}`, "g"); // {{first_name}} // {{last_name}} // {{email}}
+    content = content.replace(regex, data[i]); // Bojan // Gavrovski // bojan@gmail.com
+  }
 
   let options = {
     from: config.get("service").sender_email,
@@ -53,36 +39,21 @@ const sendMail = async (to, type, data) => {
     text: "Testing",
     html: content,
   };
-
-  console.log("options", options);
+  console.log(options);
 
   try {
-    // let res =
-    // await mg.messages.create(config.get("service").domain, options);
-    // return res;
-    mailgun.sendText(
-      "example@example.com",
-      ["Recipient 1 <rec1@example.com>", "rec2@example.com"],
-      "This is the subject",
-      "This is the text",
-      "noreply@example.com",
-      {},
-      function (err) {
-        if (err) console.log("Oh noes: " + err);
-        else console.log("Success");
-      }
-    );
-    // return test;
+    let res = await mg.messages.create(config.get("service").domain, options);
+    console.log("res", res);
   } catch (err) {
     throw err;
   }
 };
 
 const readTemplate = async (file) => {
-  return new Promise((succes, fail) => {
+  return new Promise((success, fail) => {
     fs.readFile(file, "utf-8", (err, data) => {
       if (err) return fail(err);
-      return succes(data);
+      return success(data);
     });
   });
 };
@@ -90,3 +61,18 @@ const readTemplate = async (file) => {
 module.exports = {
   sendMail,
 };
+
+// mg.messages.create(
+//     // 'https://api.mailgun.net/v3/sandboxba0709afca584019bdeef25c1b10e64f.mailgun.org',
+//     'sandboxba0709afca584019bdeef25c1b10e64f.mailgun.org',
+//     {
+//         from: 'pero@pero.com',
+//         to: 'bojan@beyondbasics.co',
+//         subject: 'Hello Pero',
+//         html: '<h1>Hello pero your email is here!</h1>'
+//     }
+// ).then(res => {
+//     console.log('SUCCESS', res);
+// }).catch(err => {
+//     console.log(err);
+// });
