07aa8e74d2765d135838c6284c839f587bcf1997
Vangel22 <h.vangel22@gmail.com>
2023-05-08 21:01:49 +0200

    send mail

--- c10/handlers/mailer.js
+++ c10/handlers/mailer.js
@@ -9,6 +9,15 @@ const sendMessage = async (req, res) => {
   }
 };
 
+const sendMail = async (req, res) => {
+  try {
+    await mailer.sendMail();
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send("Internal server error");
+  }
+};
+
 module.exports = {
   sendMessage,
 };
