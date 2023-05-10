854b77135d0b02ccc89c28caf25a6549570be0e3
Vangel22 <h.vangel22@gmail.com>
2023-05-10 20:38:48 +0200

    Setup complete

--- c11/index.js
+++ c11/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { expressjwt: jwt } = require("express-jwt");
 
 const {
   sendMessage,
@@ -6,14 +7,48 @@ const {
   sendPasswordResetMail,
 } = require("./handlers/mailer");
 const config = require("./pkg/config");
+const {
+  login,
+  register,
+  refreshToken,
+  forgotPassword,
+  resetPassword,
+} = require("./handlers/auth");
+require("./pkg/db");
 
 const api = express();
 
 api.use(express.json());
 
+api.use(
+  jwt({
+    secret: config.get("development").jwt_key,
+    algorithms: ["HS256"],
+  }).unless({
+    path: [
+      "/api/v1/auth/login",
+      "/api/v1/auth/register",
+      "/api/v1/auth/forgot-password",
+      "/api/v1/auth/reset-password",
+    ],
+  })
+);
+
+api.post("/api/v1/auth/login", login);
+api.post("/api/v1/auth/register", register);
+api.get("/api/v1/auth/refresh-token", refreshToken);
+api.post("/api/v1/auth/forgot-password", forgotPassword);
+api.post("/api/v1/auth/reset-password", resetPassword);
+
 api.post("/api/v1/sendmessage", sendMessage);
 api.post("/api/v1/sendmail", sendWelcomeMail);
 api.post("api/v1/reset-pass", sendPasswordResetMail);
+
+api.use(function (err, req, res, next) {
+  if (err.name === "UnauthorizedAccess") {
+    res.status(401).send("Invalid token...");
+  }
+});
 
 api.listen(config.get("development").port, (err) => {
   err
@@ -22,7 +57,3 @@ api.listen(config.get("development").port, (err) => {
 });
 
 //Homework
-//1. What is callback, promise, async/await
-//2. const, let, var differences
-//3. types of functions
-//4. explain crud functionalities
